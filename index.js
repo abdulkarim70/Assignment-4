92be82330c46713d2f15f25c6f86e217374aed0c
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-06-02 07:57:02 +0600

    hidden class applied by using for loop

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let currentTab='all';
 const tabActive=['bg-navy', 'border-navy', 'text-white']
 const tabInactive=['bg-transparent', 'text-slate-700','border-slate-200', 'text-black']
+const allContainer=document.getElementById('all-container');
+const interviewContainer=document.getElementById('interview-container');
+const rejectContainer=document.getElementById('reject-container');
+
 function switchTab(tab){
 const tabs=['all','interview','rejected']
 for(const t of tabs){
@@ -15,6 +19,19 @@ else{
     tabName.classList.remove(...tabActive)
     tabName.classList.add(...tabInactive)
 }
+}
+const pages=[allContainer, interviewContainer, rejectContainer]
+for(const section of pages){
+    section.classList.add('hidden');
+}
+if(tab==='all'){
+    allContainer.classList.remove('hidden');
+}
+else if(tab==='interview'){
+    interviewContainer.classList.remove('hidden')
+}
+else{
+    rejectContainer.classList.remove('hidden')
 }
 }
 switchTab(currentTab)
